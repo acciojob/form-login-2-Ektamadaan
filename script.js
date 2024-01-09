316fbdbd9e0a57e96445610e3d769911a6cf7b9c
Ektamadaan <ektamadaan45@gmail.com>
2024-01-09 14:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function dis(){
+	var fname = document.getElementById("fname").value ;
+	var lname = document.getElementById("lname").value ;
+	var pnumber = document.getElementById("pnumber").value ;
+	var email = document.getElementById("email").value ;
+	var details = "First Name: " + fname + "\nLast Name: " + lname
+	+ "\nPhone Number: " + pnumber + "\nEmail ID: " + email;
+	alert(details);
+}
